0bb8d4a56e7b6ee57269ef6a04a25d399067302a
Veysel Arslan <veysel.arslan1288@gmail.com>
2024-07-09 23:41:05 -0500

    user model

---
+++ models/users.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require('mongoose')
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        // TODO: ask teacher about matching email
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+}, {
+    toObject: {
+        virtuals: true
+    }
+})
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length
+})
+
+const User = model('User', userSchema)
+
+module.exports = User
