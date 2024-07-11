450d9f32d021c9c14dc4c246b9cbbeb7a909e666
Veysel Arslan <veysel.arslan1288@gmail.com>
2024-07-11 15:24:14 -0500

    userRoutes.js

--- models/thoughts.js
+++ models/thoughts.js
@@ -1,4 +1,4 @@
-const { Schema, model,  } = require('mongoose')
+const { Schema, model } = require('mongoose')
 function formatDate(date) {
  return date.toString()
 }
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router()
+const { User, Thought } = require('../../models')
+
+router.get('/', async (req, res) => {
+
+})
+
+router.get('/:_id', async (req, res) => {
+
+})
+
+router.post('/', async (req, res) => {
+
+})
+
+router.put('/:_id', async (req, res) => {
+
+})
+
+router.delete('/:_id', async (req, res) => {
+    
+})
