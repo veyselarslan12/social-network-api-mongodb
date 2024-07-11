267bd1c82e5615ec6d17d4471dea800ff624e7d0
Veysel Arslan <veysel.arslan1288@gmail.com>
2024-07-11 15:33:27 -0500

    userRoutes.js updated

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -2,21 +2,50 @@ const router = require('express').Router()
 const { User, Thought } = require('../../models')
 
 router.get('/', async (req, res) => {
-
+    try {
+        const users = await User.find()
+        res.json(users)
+    } catch (error) {
+        res.status(500).send('Error getting all the users.')
+    }
 })
 
 router.get('/:_id', async (req, res) => {
-
+    const { _id } = req.params
+    try {
+        const user = await User
+            .findById(_id)
+            .populate('thoughts')
+            .populate('friends')
+        res.json(user)    
+    } catch (error) {
+        res.status(500).send('Error getting the user.')
+    }
 })
 
 router.post('/', async (req, res) => {
-
+    try {
+        const newUser = await User.create(req.body)
+        res.json(newUser)
+    } catch (error) {
+        res.status(400).send('Error creating new user.')
+    }
 })
 
 router.put('/:_id', async (req, res) => {
-
+    const{ _id } = req.params
+    try {
+        
+    } catch (error) {
+        
+    }
 })
 
 router.delete('/:_id', async (req, res) => {
-    
+    const { _id } = req.params
+    try {
+        
+    } catch (error) {
+        
+    }
 })
