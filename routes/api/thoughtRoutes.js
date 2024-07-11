90ee559d542e5ff3ab7fb150b6cdb5457d6463ba
Veysel Arslan <veysel.arslan1288@gmail.com>
2024-07-11 15:43:30 -0500

    thoughtRoutes.js

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,3 +1,39 @@
 const router = require('express').Router()
 const { User, Thought } = require('../../models')
 
+router.get('/', async (req, res) => {
+    try {
+        const thoughts = await Thought.find();
+        res.json(thoughts)
+    } catch (error) {
+        res.status(500).send('Error getting all the thoughts.')
+    }
+})
+
+router.get('/:_id', async (req, res) => {
+    const{ _id } = req.params
+    try {
+        const thought = await Thought.findById(_id)
+        res.json(thought)
+    } catch (error) {
+        res.status(500).send('Error getting thought.')
+    }
+})
+
+router.post('/', async (req, res) => {
+    try {
+        const newThought = await Thought.create(req.body._id)
+
+        // await User.findByIdAndUpdate(req.body)
+    } catch (error) {
+        
+    }
+})
+
+router.put('/', async (req, res) => {
+    
+})
+
+router.delete('/', async (req, res) => {
+    
+})
