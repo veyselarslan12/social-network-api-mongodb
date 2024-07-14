434580f34bcb338dd9d09f8c40c70c96c69d9c9c
Veysel Arslan <veysel.arslan1288@gmail.com>
2024-07-13 20:56:28 -0500

    thought routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const { User, Thought } = require('../../models')
 
+// Gets all the thoughts
 router.get('/', async (req, res) => {
     try {
         const thoughts = await Thought.find();
@@ -10,6 +11,7 @@ router.get('/', async (req, res) => {
     }
 })
 
+// Gets one thought by thought's id
 router.get('/:_id', async (req, res) => {
     const{ _id } = req.params
     try {
@@ -20,6 +22,7 @@ router.get('/:_id', async (req, res) => {
     }
 })
 
+// Creates a new thought
 router.post('/', async (req, res) => {
     try {
         const newThought = await Thought.create(req.body)
@@ -33,13 +36,43 @@ router.post('/', async (req, res) => {
     }
 })
 
-router.put('/', async (req, res) => {
-    
+// Updates thought by thought's id
+router.put('/:_id', async (req, res) => {
+    const { _id } = req.params
+    try {
+        const updatedThought = await Thought.findByIdAndUpdate(_id, req.body, { new: true })
+        res.json(updatedThought)
+    } catch (error) {
+        res.status(500).send('Error updating thought.')
+    }
 })
 
-router.delete('/', async (req, res) => {
-    
+// Delete thought by thought's id
+router.delete('/:_id', async (req, res) => {
+    const { _id } = req.params
+    try {
+        const deletedThought = await Thought.findByIdAndDelete(_id)
+        // TODO: Ask teacher
+    } catch (error) {
+        res.status(500).send('Error deleting thought.')
+    }
 })
 
+router.post('/:thoughtId/reactions', async (req, res) => {
+    const { thoughtId } = req.params
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+
+router.delete('/:thoughtId/reactions/:reactionId', async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
 
 module.exports = router
